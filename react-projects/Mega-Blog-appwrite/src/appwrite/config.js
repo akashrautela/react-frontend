36fef9a401243c05ee537377f632f7c0d07fad4c
akashrautela <akashrautela007@gmail.com>
2024-04-17 22:59:43 +0530

    mega-blog-project-using-appwrite

---
+++ react-projects/Mega-Blog-appwrite/src/appwrite/config.js
@@ -0,0 +1,137 @@
+import config from "../config/config.js"
+import  {Client, Account, ID, Databases, Storage, Query} from "appwrite"
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(config.appwriteUrl)
+        .setProject(config.appwriteProjectId);
+
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);       
+    }
+
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug, 
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            );
+        } catch (error) {
+            console.log("Error on createPost ", error);
+        }
+    }
+
+    async updatePost(slug, {title,content,featuredImage,status}) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Error on updatePost : ", error);
+        }
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("error at deletepost : ",error);
+            return false;
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug
+            );
+        } catch (error) {
+            console.log("Error at getPost : ",error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try {
+            return this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Error at getPosts : ",error);
+            return false;
+        }
+    }
+
+    //file upload services
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+               config.appwriteBucketId,
+               ID.unique(),
+               file
+            )
+        } catch (error) {
+            console.log("error at uploadfile : ",error);
+            return false;
+        }        
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("error at deleteFile : ",error);
+            return false;
+        }        
+    }
+
+    async filePreview(fileId) {
+        try {
+            return await this.bucket.getFilePreview(
+                config.appwriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            console.log("error at filePreview : ",error);
+            return false;
+        }
+    }
+}
+
+
+const service = new Service();
+export default service;
