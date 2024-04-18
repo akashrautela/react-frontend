4bbe16bcab1e5f7ab7b5807647638823b63e66bd
akashrautela <akashrautela007@gmail.com>
2024-04-19 00:21:14 +0530

    integrated few more components

--- react-projects/Mega-Blog-appwrite/src/components/index.js
+++ react-projects/Mega-Blog-appwrite/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx"
+import RTE from "./RTE.jsx";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
